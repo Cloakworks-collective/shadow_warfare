4cd2cb212d1faf836f5aafb2db465af222d52b4d
enderNakamoto <ender.nakamoto@gmail.com>
2024-02-18 07:41:03 -0800

    trying to get test to work

--- contracts/test/AutoBattler.t.sol
+++ contracts/test/AutoBattler.t.sol
@@ -1,25 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.19;
 
-// import {Test, console} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {NoirProver} from "foundry-noir/Noir.sol";
 
-/* 
-contract CounterTest is Test {
-    Counter public counter;
+import {AutoBattler} from "../src/AutoBattler.sol";
+import {UltraVerifier as ArmyVerifier} from "../src/ArmyVerifier.sol";
+
+
+
+contract AutoBattlerTest is Test {
+    NoirProver public noirProver;
+    ArmyVerifier public armyVerifier;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        armyVerifier = new ArmyVerifier();
+        noirProver = new NoirProver()
+            .with_nargo_project_path("../circuits/army");
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    function testGenerateAndVerifyProof() public {
+        noirProver
+            .with_input(NoirProver.CircuitInput("army", [200, 300, 500]))
+            .with_public_input(NoirProver.CircuitInput("army_hash", 0x02b55d34358004a8e51d4403e936cfbc6f1c3fac38ffd40a30015389575f23d8));
+
+        bytes memory proof = noirProver.generate_proof();
+        armyVerifier.verifyProof(proof, [0x02b55d34358004a8e51d4403e936cfbc6f1c3fac38ffd40a30015389575f23d8]);
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }
 }
- */
